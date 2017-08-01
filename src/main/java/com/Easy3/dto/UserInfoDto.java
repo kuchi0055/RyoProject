6c21fc5798ee94c3a5d5eaade65fd7c5c573bb10
kuchi0055 <sekiguchitest@gmail.com>
2017-08-02 00:55:48 +0900

    Dto新規追加

---
+++ src/main/java/com/Easy3/dto/UserInfoDto.java
@@ -0,0 +1,29 @@
+package com.Easy3.dto;
+
+import java.io.Serializable;
+
+import org.seasar.framework.container.annotation.tiger.Component;
+import org.seasar.framework.container.annotation.tiger.InstanceType;
+
+@Component(instance = InstanceType.SESSION)
+public class UserInfoDto implements Serializable{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
