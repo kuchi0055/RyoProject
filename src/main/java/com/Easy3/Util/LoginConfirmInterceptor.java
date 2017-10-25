a0adf430be5279df564b065172cf4a2ecb89a16f
ryosekiguchi <sekiguchitest@gmail.com>
2017-10-25 18:11:49 +0900

    [fix]sql発行時にログファイルへ書き込むように修正。イベント登録画面のラジオボタンを表示するように修正。
    発行時にログファイルへ書き込むように修正。イベント登録画面のラジオボタンを表示するように修正。

--- src/main/java/com/Easy3/Util/LoginConfirmInterceptor.java
+++ src/main/java/com/Easy3/Util/LoginConfirmInterceptor.java
@@ -5,15 +5,27 @@ import org.seasar.framework.aop.interceptors.AbstractInterceptor;
 import org.seasar.struts.annotation.Execute;
 
 import com.Easy3.service.LoginService;
+import com.sun.istack.internal.logging.Logger;
 
 public class LoginConfirmInterceptor extends AbstractInterceptor{
 
 	private static final long serialVersionUID = 1L;
 
+	private Logger logger = Logger.getLogger(getClass());
 	protected LoginService loginService;
 
 	@Override
 	public Object invoke(MethodInvocation invocation) throws Throwable {
+
+		String className = getTargetClass(invocation).getName();
+	    String methodName = invocation.getMethod().getName();
+        String actionName = className + "#" + methodName;
+
+        //処理開始ログ出力
+        logger.info("\n/////////////////////////:" + actionName + "を開始します。");
+
+         //主処理
+
 		// TODO 自動生成されたメソッド・ス
 		// 両方の条件を満たしていない場合、Loginページへ飛ばす。
 				return (!isExecuteMethod(invocation) || loginService.isLoginOK()) ? invocation.proceed() : "/SC0101";
@@ -25,8 +37,19 @@ public class LoginConfirmInterceptor extends AbstractInterceptor{
 	 * @return アノテーションがついていればtrue
 	 */
 	private boolean isExecuteMethod(MethodInvocation invocation) {
+
+		String className = getTargetClass(invocation).getName();
+	    String methodName = invocation.getMethod().getName();
+        String actionName = className + "#" + methodName;
+
+        //処理開始ログ出力
+        logger.info("\n/////////////////////////:" + actionName + "を開始します。");
+
+
 		return invocation.getMethod().isAnnotationPresent(Execute.class);
 	}
+
+
 
 //	/**
 //	 * セッション上にDtoがあるか、あった場合その中にuserIDは保持されているか。
